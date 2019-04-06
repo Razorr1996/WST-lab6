ddd1db8428eb5ee9fbc78b0d0a59842c23a3db75
Artem Basalaev <basa62.1996@gmail.com>
2019-04-06 16:16:07 +0300

    Added XmlRootElement annotation to entity

--- dao/src/main/java/ru/basa62/wst/lab4/BooksEntity.java
+++ dao/src/main/java/ru/basa62/wst/lab4/BooksEntity.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@XmlRootElement
 public class BooksEntity {
     private Long id;
     private String name;
