2e238aef716eee81bf7afa18de0eb281c2131e61
Artem Basalaev <basa62.1996@gmail.com>
2019-05-07 22:23:49 +0300

    Add async print

--- client/src/main/java/ru/basa62/wst/lab6/Client.java
+++ client/src/main/java/ru/basa62/wst/lab6/Client.java
@@ -79,16 +79,22 @@ public class Client {
         if (result.isError()) {
             System.out.println(result.getMessage());
         } else {
-            List<BooksEntity> books2 = result.getResult();
-
-            if (books2.size() == 0) {
-                System.out.println("Ничего не найдено");
-            } else {
-                System.out.println("Найдено:");
-                for (BooksEntity book : books2) {
-                    System.out.println(printBook(book));
+            new Thread(() -> {
+                try {
+                    List<BooksEntity> books2 = result.getResult();
+                    Thread.sleep(5000);
+                    if (books2.size() == 0) {
+                        System.out.println("Ничего не найдено");
+                    } else {
+                        System.out.println("Найдено:");
+                        for (BooksEntity book : books2) {
+                            System.out.println(printBook(book));
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            }
+            }).start();
         }
     }
 
@@ -108,12 +114,19 @@ public class Client {
         System.out.print("ISBN: ");
         String isbn = checkEmpty(in.nextLine());
 
-        Result<String> result = booksClient.create(name, author, publicDate, isbn);
-        if (result.isError()) {
-            System.out.println(result.getMessage());
-        } else {
-            System.out.printf("Новый ID: %s", result.getResult());
-        }
+        new Thread(() -> {
+            try {
+                Result<String> result = booksClient.create(name, author, publicDate, isbn);
+                Thread.sleep(5000);
+                if (result.isError()) {
+                    System.out.println(result.getMessage());
+                } else {
+                    System.out.printf("Новый ID: %s", result.getResult());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     private static void update() {
@@ -122,10 +135,11 @@ public class Client {
         System.out.print("ID: ");
 
         String idStr = checkEmpty(in.nextLine());
-        Long id = null;
+        Long id_ = null;
         if (idStr != null) {
-            id = Long.parseLong(idStr);
+            id_ = Long.parseLong(idStr);
         }
+        Long id = id_;
 
         System.out.print("Название: ");
         String name = checkEmpty(in.nextLine());
@@ -139,12 +153,19 @@ public class Client {
         System.out.print("ISBN: ");
         String isbn = checkEmpty(in.nextLine());
 
-        Result<String> result = booksClient.update(id, name, author, publicDate, isbn);
-        if (result.isError()) {
-            System.out.println(result.getMessage());
-        } else {
-            System.out.printf("Обновлено: %s", result.getResult());
-        }
+        new Thread(() -> {
+            try {
+                Result<String> result = booksClient.update(id, name, author, publicDate, isbn);
+                Thread.sleep(5000);
+                if (result.isError()) {
+                    System.out.println(result.getMessage());
+                } else {
+                    System.out.printf("Обновлено: %s", result.getResult());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     private static void delete() {
@@ -155,12 +176,19 @@ public class Client {
         if (idStr != null) {
             long id = Long.parseLong(idStr);
 
-            Result<String> result = booksClient.delete(id);
-            if (result.isError()) {
-                System.out.println(result.getMessage());
-            } else {
-                System.out.printf("Удалено: %s", result.getResult());
-            }
+            new Thread(() -> {
+                try {
+                    Result<String> result = booksClient.delete(id);
+                    Thread.sleep(5000);
+                    if (result.isError()) {
+                        System.out.println(result.getMessage());
+                    } else {
+                        System.out.printf("Удалено: %s", result.getResult());
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         } else {
             System.out.println("Ничего не введено");
         }
